736e9b995271a96f782f2a183e6e14c5967bda72
noaloehrer <noa.loehrer@optimatik.ch>
2024-07-16 10:42:08 +0200

    created all components

---
+++ ArcheryForum/src/app/content/posts/new-topic/new-topic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-topic',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-topic.component.html',
+  styleUrl: './new-topic.component.css'
+})
+export class NewTopicComponent {
+
+}
