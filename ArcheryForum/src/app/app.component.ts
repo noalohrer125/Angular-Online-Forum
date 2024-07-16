2a9e8e2fe6e920561574a3358c21e8585163d2da
noaloehrer <noa.loehrer@optimatik.ch>
2024-07-16 11:13:32 +0200

    positioning of all components

--- ArcheryForum/src/app/app.component.ts
+++ ArcheryForum/src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationbarComponent } from "./navigationbar/navigationbar.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./content/home/home.component";
+import { PostsComponent } from "./content/posts/posts.component";
+import { MyAccountComponent } from "./content/my-account/my-account.component";
+import { PostComponent } from "./content/posts/post/post.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationbarComponent],
+  imports: [
+    RouterOutlet,
+    NavigationbarComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PostsComponent,
+    MyAccountComponent,
+    PostComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ArcheryForum';
+
+  content: string = 'app-home'
 }
