21002e55fe82f83378ffb83d94b9375cb487c714
noaloehrer <noa.loehrer@optimatik.ch>
2024-07-17 09:59:08 +0200

    added routing for home, posts and my-account and small style changes

--- ArcheryForum/src/app/app.routes.ts
+++ ArcheryForum/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+// app-routes.ts
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./content/home/home.component";
+import { MyAccountComponent } from "./content/my-account/my-account.component";
+import { PostsComponent } from "./content/posts/posts.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'posts', component: PostsComponent },
+    { path: 'my-account', component: MyAccountComponent },
+    // Other routes below if needed
+];
--- ArcheryForum/src/app/navigationbar/navigationbar.component.ts
+++ ArcheryForum/src/app/navigationbar/navigationbar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navigationbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './navigationbar.component.html',
   styleUrl: './navigationbar.component.css',
 })
