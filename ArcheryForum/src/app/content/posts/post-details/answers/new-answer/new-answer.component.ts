b02bcad078a1627dac5b05e876d9c8697f79a3c6
noaloehrer <noa.loehrer@optimatik.ch>
2024-07-17 07:53:02 +0200

    added post-details.component, answers.component and new-answer.component

---
+++ ArcheryForum/src/app/content/posts/post-details/answers/new-answer/new-answer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-answer',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-answer.component.html',
+  styleUrl: './new-answer.component.css'
+})
+export class NewAnswerComponent {
+
+}
