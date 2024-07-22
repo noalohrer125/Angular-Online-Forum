d363b604c913a5bcbc0feb779370fad8e784b635
noaloehrer <noa.loehrer@optimatik.ch>
2024-07-22 15:45:30 +0200

    dynamically render answers to posts

--- ArcheryForum/src/app/content/posts/post-details/answers/answer/answer.component.ts
+++ ArcheryForum/src/app/content/posts/post-details/answers/answer/answer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Answer } from '../../../../../shared/interfaces';
 
 @Component({
   selector: 'app-answer',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './answer.component.css'
 })
 export class AnswerComponent {
-
+  @Input() answer?: Answer;
 }
--- ArcheryForum/src/app/content/posts/post-details/answers/answers.component.ts
+++ ArcheryForum/src/app/content/posts/post-details/answers/answers.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AnswerComponent } from "./answer/answer.component";
 import { NewAnswerComponent } from "./new-answer/new-answer.component";
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Answer } from '../../../../shared/interfaces';
 
 @Component({
   selector: 'app-answers',
@@ -10,10 +12,17 @@ import { RouterLink } from '@angular/router';
     AnswerComponent,
     NewAnswerComponent,
     RouterLink,
+    CommonModule,
 ],
   templateUrl: './answers.component.html',
   styleUrl: './answers.component.css'
 })
 export class AnswersComponent {
+  Answers: Answer[] = JSON.parse(localStorage.getItem('Answers') || '[]');
 
+  ngOnInit() {
+    const id:number = Number(localStorage.getItem('CurrentPost'))
+
+    this.Answers = this.Answers.filter(item => item.post_id !== id);
+  }
 }
